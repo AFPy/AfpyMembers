5f28809ce8f5d34e1b90409ffb9192642131e966
Gael Pasgrimaud <gael@gawel.org>
2013-04-26 14:09:54 +0200

    fix import error

--- members/controllers/maps.py
+++ members/controllers/maps.py
@@ -1,11 +1,8 @@
-import re
-import urllib
+import os
 import logging
-from datetime import datetime
 from cPickle import load
-from members.lib.base import *
+from members.lib.base import *  # NOQA
 from afpy.core import config
-from afpy.core.countries import COUNTRIES
 from pylons.decorators.cache import beaker_cache
 
 log = logging.getLogger(__name__)
@@ -26,8 +23,9 @@ class MapsController(BaseController):
         conn = ldap.get_conn()
         users = []
         for i in range(1, 10):
-            users.extend(conn.search_nodes(filter='(&(postalCode=%s*)(street=*))' % i,
-                        attrs=['uid', 'street', 'postalCode', 'l', 'st']))
+            users.extend(conn.search_nodes(
+                filter='(&(postalCode=%s*)(street=*))' % i,
+                attrs=['uid', 'street', 'postalCode', 'l', 'st']))
         datas = {}
         if os.path.isfile(filename):
             coords = load(open(filename))
@@ -36,16 +34,17 @@ class MapsController(BaseController):
         for user in users:
             try:
                 short_address = u'%s, %s' % (
-                        user.postalCode.strip(),
-                        user.st.strip())
+                    user.postalCode.strip(),
+                    user.st.strip())
             except UnicodeDecodeError, e:
                 log.error('%r %s - %s', e, e, user)
                 coord = None
             else:
                 coord = coords.get(short_address)
             if coord:
-                datas[short_address] = datas.get(short_address, []) + [user.uid]
-        return {'result':[dict(point=[coords[k].get('lng'), coords[k].get('lat')], address=k,
-                     users=''.join(['<div>%s</div>' % u for u in v])
-                     ) for k, v in datas.items()]}
-
+                datas[short_address] = datas.get(short_address, []) + \
+                    [user.uid]
+        return {'result': [dict(point=[coords[k].get('lng'),
+                                coords[k].get('lat')], address=k,
+                                users=''.join(['<div>%s</div>' % u for u in v])
+                                ) for k, v in datas.items()]}
